fa7a2c502b2e5e0b2b2b4603c83a7b9d7a617026
Stefania <126720391+Aoblu87@users.noreply.github.com>
2024-04-09 16:39:58 +0200

    install

---
+++ src/components/DeleteContact/DeleteContact.jsx
@@ -0,0 +1,86 @@
+import React, { forwardRef } from "react"
+import { ToastContainer, toast } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+import { usePage } from "../../context/PageContext"
+import useContacts from "../../hooks/fetchData"
+
+const DeleteContact = forwardRef((props, ref) => {
+    const {
+        selectedContactId,
+        active,
+        activeClass,
+        noActiveClass,
+        customClass,
+    } = props
+    const { currentPage } = usePage()
+
+    const { fetchContacts } = useContacts(currentPage)
+
+    //Confirm before deleting
+    const handleConfirm = async () => {
+        if (!selectedContactId) {
+            return
+        }
+        if (window.confirm("Are you sure you want to delete the contact?")) {
+            await handleDelete()
+        }
+    }
+    //Request to delete contact
+    const handleDelete = async () => {
+        let formData = new FormData()
+        formData.append("id", selectedContactId)
+
+        const fetchPromise = new Promise(async (resolve, reject) => {
+            try {
+                const response = await fetch(
+                    "https://tjf-challenge.azurewebsites.net/web/people/delete",
+                    {
+                        method: "DELETE",
+                        body: formData,
+                    }
+                )
+
+                if (response.ok) {
+                    const data = await response.json()
+                    resolve(data)
+                    // fetchContacts()
+                } else {
+                    reject(new Error(`HTTP error! Status: ${response.status}`))
+                }
+            } catch (error) {
+                reject(error)
+            }
+        })
+
+        toast.promise(fetchPromise, {
+            pending: "Loading...",
+            success: "Deleted!",
+            error: "Something went wrong",
+        })
+    }
+    return (
+        <>
+            <button
+                onClick={handleConfirm}
+                className={`${
+                    active ? activeClass : noActiveClass
+                } hover:bg-subdue hover:text-neutral  ${customClass}`}
+            >
+                Delete
+            </button>
+            <ToastContainer
+                position="bottom-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
+        </>
+    )
+})
+export default DeleteContact
